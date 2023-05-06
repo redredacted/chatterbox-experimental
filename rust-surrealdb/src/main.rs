92df840b34181c4f06719ba037344902118f6a97
V0idMatr1x <v0idmatr1x.0@gmail.com>
2023-05-05 23:38:53 -0500

    update: db and grpc server

--- rust-surrealdb/src/main.rs
+++ rust-surrealdb/src/main.rs
@@ -1,4 +1,6 @@
+#[allow(unused_imports)]
 use std::env;
+mod db;
 
 use anyhow::Result;
 
@@ -10,49 +12,9 @@ use surrealdb::opt::auth::Root;
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
 
-#[derive(Debug, Serialize)]
-struct Name<'a> {
-    first: &'a str,
-    last: &'a str,
-}
+use db::auth;
 
-#[derive(Debug, Serialize)]
-struct Person<'a> {
-    title: &'a str,
-    name: Name<'a>,
-    marketing: bool,
-}
 
-#[derive(Debug, Serialize)]
-struct Responsibility {
-    marketing: bool,
-}
-
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[allow(dead_code)]
-    id: Thing,
-}
-
-struct DbAuth<'a> {
-    username: &'a str,
-    password: &'a str,
-}
-
-impl<'a> DbAuth{
-    fn new() -> Self {
-        dotenv().expect(".env file not found");
-        let db_user: &str = env::var("USERNAME").expect("No Username in env file").as_str();
-        let db_pass: &str = env::var("PASSWORD").expect("No Password in env file").as_str();
-
-        Self {
-            username: db_user,
-            password: db_pass,
-        }
-    }
-
-  
-}
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
@@ -71,31 +33,7 @@ async fn main() -> surrealdb::Result<()> {
     db.use_ns("test").use_db("chatterbox_db_test").await?;
 
     // Create a new person with a random id
-    let created: Record = db.create("person").content(Person {
-        title: "Founder & CEO",
-        name: Name {
-            first: "Tobie",
-            last: "Morgan Hitchcock",
-        },
-        marketing: true,
-    }).await?;
-    dbg!(created);
-
-    // Update a person record with a specific id
-    let updated: Record = db
-        .update(("person", "jaime"))
-        .merge(Responsibility { marketing: true }).await?;
-    dbg!(updated);
-
-    // Select all people records
-    let people: Vec<Record> = db.select("person").await?;
-    dbg!(people);
-
-    // Perform a custom advanced query
-    let groups = db
-        .query("SELECT marketing, count() FROM type::table($table) GROUP BY marketing")
-        .bind(("table", "person")).await?;
-    dbg!(groups);
+    
 
     Ok(())
 }
