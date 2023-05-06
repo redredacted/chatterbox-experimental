9aba49b77a837f8b447267efee4079763f14e90d
V0idMatr1x <v0idmatr1x.0@gmail.com>
2023-05-06 17:24:27 -0500

    update: add logger to all go micro-services

---
+++ go-oauth/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+)
+
+func logConfig() (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{"./log/log.txt"}
+	
+	return cfg.Build()
+}
+
+func FLogFatal(err error) {
+	logger, _ := logConfig()
+	
+	defer logger.Sync()
+	logger.Fatal(err.Error())
+}
+
+func FLogInfo(msg string) {
+	logger, _ := logConfig()
+	defer logger.Sync()
+	
+	logger.Info(msg)
+}
+
+func FLogDebug(msg string) {
+	logger, _ := logConfig()
+	defer logger.Sync()
+	
+	logger.Debug(msg)
+}
+
+func LogStdout() *zap.Logger {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()	
+	
+	return logger
+}
